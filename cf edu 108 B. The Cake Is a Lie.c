19728d44a2290e8ffcc900047ac42cdeadc1316d
piash76 <48187014+piash76@users.noreply.github.com>
2021-04-30 11:18:10 +0600

    Add files via upload

---
+++ cf edu 108 B. The Cake Is a Lie.c
@@ -0,0 +1,61 @@
+
+///Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+
+#define ll long long
+#define pll  pair<ll,ll>
+
+#define ff    first
+#define ss    second
+
+#define  mp  make_pair
+#define  pb  push_back
+
+#define cy  cout<<"YES"<<endl
+#define cn   cout<<"NO"<<endl
+
+
+using namespace std;
+
+const ll inf=1e18;
+const int mod=1e9+7;
+const int mod2=2147483647; ///2^31-1
+const int M=500005;
+
+ll a[M];
+
+
+int main()
+{
+
+
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+
+
+    ll t,i,j;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m,k;cin>>n>>m>>k;
+
+        ll s=(n-1)+(m-1)*n;
+
+        //s=min(s,(m-1)+(n-1)*m);
+
+       // ll x=
+       // s=min(s,)
+       if(s==k) cy;
+       else cn;
+
+
+    }
+
+    return 0;
+
+}
+
+
+
+
